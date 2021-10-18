fcb8256fa60a06e5e40dc54fb02c0ea34cf55215
Nguyen Lam Khoa 20206560 <khoa.nl206560@sis.hust.edu.vn>
2021-10-18 23:40:46 +0700

    update

---
+++ 4-10-2021/Exam4/Exam4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exam4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BookList bookList = new BookList();
+            bookList.InputList();
+            bookList.ShowList();
+
+        }
+    }
+}
